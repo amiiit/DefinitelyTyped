41713d483fa3c4e57abca938d0dc3738ad6f2adb
Amit Jakubowicz <a.jakubowicz@travelaudience.com>
2018-07-31 14:49:24 +0200

    Add readOnly attribute on React select element

---
+++ types/react/test/htmlAttributes.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const htmlSelect = <select
+  name="foo"
+  multiple={false}
+  required={false}
+  disabled={true}
+  readOnly={true}
+  onBlur={this.props.onBlur}
+  value={this.props.value}
+ />;
